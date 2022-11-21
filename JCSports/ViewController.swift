48fa43f257e65e480db2fde10ec04a12ba8708de
ply2782 <ply2782@daum.net>
2022-11-21 18:03:31 +0900

    Initial Commit

---
+++ JCSports/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JCSports
+//
+//  Created by 바틀 on 2022/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JCSportsUITests/JCSportsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  JCSportsUITestsLaunchTests.swift
+//  JCSportsUITests
+//
+//  Created by 바틀 on 2022/11/21.
+//
+
+import XCTest
+
+class JCSportsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
